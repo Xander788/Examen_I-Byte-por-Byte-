eb18aa7d6776de33781cbfd6fb79ce195a6ab193
Alexander <alramirezba@est.utn.ac.cr>
2025-07-03 14:28:22 -0600

    Clase base Persona
    Se crea la clase base Persona como abstract y se crean sus metodos get, su constructor y su to string.

---
+++ src/Personas/Persona.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+/**
+ *
+ * @author Student
+ */
+public abstract class Persona {
+    protected int cedula;
+    protected String nombre;
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Persona(int cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "cedula=" + cedula + ", nombre=" + nombre + '}';
+    }   
+}
